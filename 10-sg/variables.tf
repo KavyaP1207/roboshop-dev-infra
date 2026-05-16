659d233b8b0961a6ff4439956f3e6bc491b61a58
kavya Poreddy <poreddykavya1207@gmail.com>
2026-05-16 14:45:54 +0530

    terraform

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -5,3 +5,18 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "sg_names" {
+  default = [
+    #database
+    "mongodb", "redis", "mysql", "rabbitmq",
+    #backend
+    "catalogue", "user", "cart", "shipping", "payment",
+    #frontend
+    "frontend",
+    #bastion
+    "bastion",
+    # frontend load balancer
+    "frontend-lb"
+    ]
+}
