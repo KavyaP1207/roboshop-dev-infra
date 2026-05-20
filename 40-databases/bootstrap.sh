31c01bdf9c943a94d823cb3df8e450bef099519a
kavya Poreddy <poreddykavya1207@gmail.com>
2026-05-20 22:38:20 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-dnf install ansible -y
+# install required tools
+sudo yum install -y git
+sudo yum install -y ansible
 
+# run ansible
 ansible-pull -i localhost, \
   -U https://github.com/KavyaP1207/ansible-roboshop_roles-tf.git \
   -e component=mongodb \
