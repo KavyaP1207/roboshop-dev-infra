60ddedb274ebdaea3ab7b43b75d91c908a54cf3f
kavya Poreddy <poreddykavya1207@gmail.com>
2026-05-21 21:23:30 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/KavyaP1207/ansible-roboshop_roles-tf.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/KavyaP1207/ansible-roboshop_roles-tf.git -e component=$component main.yaml
