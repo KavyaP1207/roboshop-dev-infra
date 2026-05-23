7bef0b86784b5084f61692c2c6f0bc5e1e59f64c
kavya Poreddy <poreddykavya1207@gmail.com>
2026-05-23 22:19:45 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -122,7 +122,7 @@ resource "aws_instance" "mysql" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.mysql_sg_id]
   subnet_id = local.database_subnet_id
-  iam_instance_profile = ws_iam_instance_profile.mysql
+  iam_instance_profile = aws_iam_instance_profile.mysql
   tags = merge(
     local.common_tags,
     {
