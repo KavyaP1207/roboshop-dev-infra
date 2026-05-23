b62bc769b979926e48df9be63c49236de7f10f38
kavya Poreddy <poreddykavya1207@gmail.com>
2026-05-23 12:12:47 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -3,3 +3,21 @@
 component=$1
 dnf install ansible -y
 ansible-pull -U https://github.com/KavyaP1207/ansible-roboshop_roles-tf.git -e component=$component main.yaml
+
+REPO_URL=https://github.com/KavyaP1207/ansible-roboshop_roles-tf.git
+REPO_DIR=/opt/roboshop/ansible
+ANSIBLE_DIR=ansible-roboshop_roles-tf
+
+mkdir -p $REPO_DIR
+mkdir -p /var/log/roboshop/
+touch ansible.log
+
+cd $REPO_DIR
+if [ -d $ANSIBLE_DIR ]; then
+    cd $ANSIBLE_DIR 
+    git pull
+else
+    git clone $REPO_URL
+    cd $ANSIBLE_DIR
+fi
+ansible-playbook -e component= $component main.yaml
