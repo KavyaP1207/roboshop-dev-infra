ffd63d0c8d4f81b5ed7cef40c11c30e765cba7b9
kavya Poreddy <poreddykavya1207@gmail.com>
2026-05-24 14:17:07 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,7 +1,7 @@
 locals {
   common_name_suffix = "${var.project_name}-${var.environment}"
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-  private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_id.value)[0]
   ami_id = data.aws_ami.joindevops.id
   common_tags = {
     Project = var.project_name
