28d270d73184f8472da373b878cd1ca4c6c7d135
kavya Poreddy <poreddykavya1207@gmail.com>
2026-05-30 17:39:28 +0530

    terraform

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -29,8 +29,8 @@ data "aws_ssm_parameter" "mysql_sg_id" {
   name = "/${var.project_name}/${var.environment}/mysql_sg_id"
 }
 
-data "aws_ssm_parameter" "database_subnet_id" {
-  name  = "/${var.project_name}/${var.environment}/database_subnet_id"
+data "aws_ssm_parameter" "database_subnet_ids" {
+  name  = "/${var.project_name}/${var.environment}/database_subnet_ids"
 }
 
 
